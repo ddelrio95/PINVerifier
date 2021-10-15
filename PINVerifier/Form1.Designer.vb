a563941e0e4db1a239767960ea0a37e168ea33ce
Dario del Rio <57421573+ddelrio95@users.noreply.github.com>
2021-10-14 20:53:57 -0400

    Add project files.

---
+++ PINVerifier/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TxtBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TxtBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(66, 36)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(326, 112)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Enter the PIN"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter 7 digits"
+        '
+        'TxtBox1
+        '
+        Me.TxtBox1.Location = New System.Drawing.Point(55, 51)
+        Me.TxtBox1.MaxLength = 7
+        Me.TxtBox1.Name = "TxtBox1"
+        Me.TxtBox1.Size = New System.Drawing.Size(192, 20)
+        Me.TxtBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(68, 179)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(72, 27)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Verify"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(184, 179)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(86, 27)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(303, 179)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 27)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(468, 235)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "PIN Verifier"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents TxtBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ PINVerifier/Form1.vb
@@ -0,0 +1,107 @@
+﻿Public Class Form1
+
+    Dim intMin() As Integer
+    Dim intMax() As Integer
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        intMin = New Integer() {7, 5, 0, 0, 6, 3, 4}
+        intMax = New Integer() {9, 7, 4, 9, 9, 6, 8}
+    End Sub
+
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        End
+    End Sub
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        TxtBox1.Text = ""
+        TxtBox1.Focus()
+    End Sub
+
+    
+
+    Private Sub TxtBox1_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TxtBox1.KeyPress
+        If Not (Char.IsDigit(e.KeyChar)) And e.KeyChar <> ChrW(Keys.Back) Then
+            e.Handled = True
+        End If
+    End Sub
+
+
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+
+        Dim violatednumber As String = ""
+        Dim textboxaray() As Char = TxtBox1.Text.ToCharArray
+
+        If ((TxtBox1.Text = "") And (textboxaray.Length < 6)) Then
+            MessageBox.Show("please enter 7  digits ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+
+            If (textboxaray.Length < 6) Then
+
+                MessageBox.Show("please enter 7  digits you not enter 7 digits  ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                If (Val(textboxaray(0)) >= intMin(0)) And (Val(textboxaray(0)) <= intMax(0)) Then
+                Else
+
+
+                    violatednumber += "1" + " ,"
+                End If
+
+                If (Val(textboxaray(1)) >= intMin(1)) And (Val(textboxaray(1)) <= intMax(1)) Then
+
+                Else
+
+                    violatednumber += "2" + " ,"
+                End If
+
+                If (Val(textboxaray(2)) >= intMin(2)) And (Val(textboxaray(2)) <= intMax(2)) Then
+
+                Else
+
+                    violatednumber += "3" + " ,"
+                End If
+
+                If (Val(textboxaray(3)) >= intMin(3)) And (Val(textboxaray(3)) <= intMax(3)) Then
+
+                Else
+                    violatednumber += "4" + " ,"
+                End If
+
+                If (Val(textboxaray(4)) >= intMin(4)) And (Val(textboxaray(4)) <= intMax(4)) Then
+
+                Else
+
+                    violatednumber += "5" + " ,"
+                End If
+                If (Val(textboxaray(5)) >= intMin(5)) And (Val(textboxaray(5)) <= intMax(5)) Then
+
+                Else
+
+
+                    violatednumber += "6" + " ,"
+                End If
+
+                If (Val(textboxaray(6)) >= intMin(6)) And (Val(textboxaray(6)) <= intMax(6)) Then
+
+                Else
+
+                    violatednumber += "7"
+                End If
+
+
+                If (violatednumber.Length > 0) Then
+
+                    MessageBox.Show("that digit  " + violatednumber + " violate the rules", "violated number", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Else
+                    MessageBox.Show("All entries are valid", "Pin Number", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            End If
+        End If
+
+    End Sub
+End Class
